a7f4a555d700e8f2cd58417b1d7fde414dc80500
imranrtr-prog <imranrtr@gmail.com>
2026-06-13 17:11:18 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Building basic calculator using python 
+
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y == 0:
+        return float('inf')
+    return x / y
+
+
+
+
+
